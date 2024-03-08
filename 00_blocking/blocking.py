3a1aee2f7323b822cdf84143dac71553465aa64c
Oliver Kasten <oliver.kasten@hslu.ch>
2024-03-08 06:56:24 +0100

    initial version

---
+++ 00_blocking/blocking.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import time
+
+
+if __name__ == '__main__':
+    print("Hit Enter to continue...")
+    input()
+    print("Waiting for 3 seconds...")
+    time.sleep(3)
+    print("Okay, bye now!")
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # distsys_24fs
 Dateien für den Kommunikations-Grundlagen-Teil von DIST_SYS
+
+Um das Repository zu clonen:
+> git clone https://gitlab.enterpriselab.ch/okasten/DIST_SYS.git
